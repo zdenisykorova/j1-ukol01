69384df49d74543777d6d83723508ffa2e89a703
zdeni sykorova <sykorova@hotmail.com>
2025-04-20 08:53:39 +0200

    úkol část 2 - prasátko, obrazce a trochu křivé slunce

--- src/main/java/cz/czechitas/java/ukol01/HlavniProgram.java
+++ src/main/java/cz/czechitas/java/ukol01/HlavniProgram.java
@@ -14,7 +14,16 @@ public class HlavniProgram {
 
         //TODO implementace domácího úkolu
 nakresliPrasatko();
-    }
+
+    nakresliObrazce();
+
+        for (int i = 0; i < 13; i++) {
+            nakresliObrazce2();
+        }
+
+        }
+
+
         public void nakresliPrasatko() {
         zofka.turnLeft(90);
         for (int i = 0; i < 4; i++) {
@@ -65,7 +74,54 @@ nakresliPrasatko();
         zofka.penDown();
         zofka.turnRight(45);
         zofka.move(20);
-        zofka.turnLeft(45);
-    }
-}
+        }
+
+        public void nakresliObrazce() {
+    zofka.penUp();
+    zofka.turnRight(45);
+    zofka.move(80);
+    zofka.penDown();
+            for (int i = 0; i < 8 ; i++) {
+                zofka.move(20);
+                zofka.turnRight(45);
+                zofka.move(20);
+            }
+
+zofka.penUp();
+    zofka.move(120);
+    zofka.penDown();
+
+    //kruh
+            for (int i = 0; i < 72; i++) {
+                zofka.turnRight(5);
+                zofka.move(4);
+            }
+
+zofka.penUp();
+            zofka.turnLeft(20);
+            zofka.move(110);
+            zofka.penDown();
+
+            //kruh
+            for (int i = 0; i < 72; i++) {
+                zofka.turnRight(5);
+                zofka.move(4);
+            }
+              }
+                //paprsky
+                public void nakresliObrazce2() {
+        zofka.penDown();
+        zofka.turnLeft(90);
+                    zofka.move(15);
+                    zofka.penUp();
+                    zofka.turnRight(180);
+                    zofka.move(15);
+                    zofka.turnLeft(90);
+
+                    for (int i = 0; i < 7; i++) {
+                        zofka.move(3);
+                        zofka.turnRight(4);
+                    }
+                    }
+                }
 
